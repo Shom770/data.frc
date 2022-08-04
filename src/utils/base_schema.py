af8771a2a7b49437100714c0de75aa808117a07c
Shom770 <82843611+Shom770@users.noreply.github.com>
2022-08-04 17:50:15 -0400

    create structure of Team and add BaseSchema

---
+++ src/utils/base_schema.py
@@ -0,0 +1,14 @@
+class BaseSchema:
+    """Base class for all schemas."""
+
+    def __init__(self, **kwargs):
+        attributes_formatted = ""
+
+        for attr_name, attr_value in kwargs.items():
+            setattr(self, attr_name, attr_value)
+            attributes_formatted += f"{attr_name}={attr_value}, "
+
+        self._attributes_formatted = attributes_formatted.rstrip(", ")
+
+    def __repr__(self):
+        return f"{type(self).__name__}({self._attributes_formatted})"
---
+++ src/utils/team.py
@@ -0,0 +1,8 @@
+from base_schema import BaseSchema
+
+__all__ = ["Team"]
+
+
+class Team(BaseSchema):
+    """Class represening a team's metadata with methods to get team specific data."""
+
