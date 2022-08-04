99ceda3edb081e1fbd4a8e2aa32c99ca2b14a502
Shom770 <82843611+Shom770@users.noreply.github.com>
2022-08-04 18:06:14 -0400

    move synchronous to functions.py

---
+++ src/utils/functions.py
@@ -0,0 +1,34 @@
+import functools
+import typing
+
+import aiohttp
+
+__all__ = ["synchronous"]
+
+
+def synchronous(coro: typing.Callable) -> typing.Callable:
+    """
+    Decorator that wraps an asynchronous function around a synchronous function.
+    Users can call the function synchronously although its internal behavior is asynchronous for efficiency.
+
+    Parameters:
+        coro: A coroutine that is passed into the decorator.
+
+    Returns:
+        A synchronous function with its internal behavior being asynchronous.
+    """
+
+    @functools.wraps(coro)
+    def wrapper(self, *args, **kwargs) -> typing.Any:
+        if not self.session:
+            self.session = aiohttp.ClientSession()
+
+        result = self._loop.run_until_complete(coro(self, *args, **kwargs))
+
+        if not self._persistent_session:
+            self._loop.run_until_complete(self.session.close())
+            self.session = None
+
+        return result
+
+    return wrapper
