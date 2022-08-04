794c10e22f83d3552fa9c5e3efce2a41645569ef
Shom770 <82843611+Shom770@users.noreply.github.com>
2022-08-04 19:00:27 -0400

    fix set issues

--- src/schemas/team.py
+++ src/schemas/team.py
@@ -4,7 +4,10 @@ from .base_schema import BaseSchema
 class Team(BaseSchema):
     """Class represening a team's metadata with methods to get team specific data."""
 
-    def __hash__(self):
+    def __eq__(self, other) -> bool:
+        return self.team_number == other.team_number
+
+    def __hash__(self) -> int:
         return self.team_number
 
     def __lt__(self, other: "Team") -> bool:
