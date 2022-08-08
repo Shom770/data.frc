e80922a0902ae1cb9f8936593343aa14ebd93c5f
Shom770 <82843611+Shom770@users.noreply.github.com>
2022-08-07 20:02:59 -0400

    update robot schema

--- src/schemas/robot.py
+++ src/schemas/robot.py
@@ -1,3 +1,5 @@
+import typing
+
 from .base_schema import BaseSchema
 
 try:
@@ -10,9 +12,9 @@ class Robot(BaseSchema):
     """Class representing a robot containing methods to get specific district information."""
 
     def __init__(self, **kwargs):
-        self.year: int = kwargs["year"]
-        self.robot_name: str = kwargs["robot_name"]
+        self.year: typing.Optional[int] = kwargs.get("year")
+        self.robot_name: typing.Optional[str] = kwargs.get("robot_name")
         self.key: str = kwargs["key"]
-        self.team_key: str = kwargs["team_key"]
+        self.team_key: typing.Optional[str] = kwargs.get("team_key")
 
         super().__init__(kwargs)
