3b81aa5bdce6f2a1a70f1414a5bc2be1d3647ae1
Shom770 <82843611+Shom770@users.noreply.github.com>
2022-08-07 20:06:42 -0400

    finished district schema

--- src/schemas/district.py
+++ src/schemas/district.py
@@ -1,3 +1,5 @@
+import typing
+
 from .base_schema import BaseSchema
 
 try:
@@ -8,3 +10,11 @@ except ImportError:
 
 class District(BaseSchema):
     """Class representing a district containing methods to get specific district information."""
+
+    def __init__(self, **kwargs):
+        self.abbreviation: typing.Optional[str] = kwargs.get("abbreviation")
+        self.display_name: typing.Optional[str] = kwargs.get("display_name")
+        self.key: str = kwargs["key"]
+        self.year: typing.Optional[int] = kwargs.get("year")
+
+        super().__init__(kwargs)
