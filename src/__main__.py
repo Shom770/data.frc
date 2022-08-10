580765aff12653abff69ece5b2e378052ab96196
Shom770 <82843611+Shom770@users.noreply.github.com>
2022-08-10 14:41:22 -0400

    matches work

--- src/__main__.py
+++ src/__main__.py
@@ -5,6 +5,7 @@ from schemas import *
 def main():
     with ApiClient() as api_client:
         team4099 = Team(key="frc4099")
-        print([match.actual_time.year for match in team4099.matches(year=range(2010, 2022))])
+        print(team4099.matches(year=2022, keys=True))
+
 
 main()
