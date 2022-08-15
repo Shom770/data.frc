8f27f8111ce705c7cb11d7c3b83af2427fe02f91
Shom770 <82843611+Shom770@users.noreply.github.com>
2022-08-15 19:00:30 -0400

    district points done

--- src/__main__.py
+++ src/__main__.py
@@ -5,7 +5,7 @@ from schemas import *
 def main():
     with ApiClient():
         ranks = Event(key="2022chcmp").district_points()
-        print(ranks)
+        print(dict(sorted(ranks.points.items(), key=lambda x: x[1]["total"])))
 
 
 main()
