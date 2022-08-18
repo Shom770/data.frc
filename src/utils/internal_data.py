eeeba6f6457533b2b66f8a0dba7a9ed3c9ccb3ba
Shom770 <82843611+Shom770@users.noreply.github.com>
2022-08-18 00:49:00 -0400

    setup get func

--- src/utils/internal_data.py
+++ src/utils/internal_data.py
@@ -2,9 +2,27 @@ import asyncio
 
 import aiohttp
 
+from .functions import synchronous
+
 
 class InternalData:
     """Contains internal attributes such as the event loop and the client session."""
 
     loop = asyncio.get_event_loop()
     session = aiohttp.ClientSession()
+
+    @synchronous
+    async def get(self, *, url: str, headers: dict) -> aiohttp.ClientResponse:
+        """
+        Sends a GET request to the TBA API.
+
+        Parameters:
+            url:
+                A string representing which URL to send a GET request to.
+            headers:
+                A dictionary containing the API key to authorize the request.
+
+        Returns:
+            An aiohttp.ClientResponse object representing the response the GET request returned.
+        """
+        response = self.session.get(url=url, headers=headers)
