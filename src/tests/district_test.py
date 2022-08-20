eb3e158c259517f0ef8af724e1a6d7c5343403d2
Shom770 <82843611+Shom770@users.noreply.github.com>
2022-08-20 18:58:12 -0400

    Add first test

--- src/tests/district_test.py
+++ src/tests/district_test.py
@@ -3,3 +3,10 @@ import pytest
 from ..api_client import ApiClient
 from ..schemas import *
 from ..utils import *
+
+
+def test_district_year_abbreviation():
+    """Tests initializing `District` via passing in the year and then the abbreviation (eg District(2022, 'chs'))"""
+    with ApiClient():
+        chs_district = District(2022, "chs")
+        assert chs_district.year == 2022 and chs_district.abbreviation == "chs" and chs_district.key == "2022chs"
