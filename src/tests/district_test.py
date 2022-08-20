5066e56013a511baa292a47161c742db8efd7d26
Shom770 <82843611+Shom770@users.noreply.github.com>
2022-08-20 19:25:26 -0400

    events and events simple tests

--- src/tests/district_test.py
+++ src/tests/district_test.py
@@ -24,3 +24,17 @@ def test_district_kwargs():
     with ApiClient():
         chs_district = District(key="2022chs")
         assert chs_district.year == 2022 and chs_district.abbreviation == "chs" and chs_district.key == "2022chs"
+
+
+def test_district_events():
+    """Tests TBA's endpoint to retrieve all events that occurred in a district."""
+    with ApiClient():
+        chs_events = District(2022, "chs").events()
+        assert isinstance(chs_events, list) and all(isinstance(event, Event) for event in chs_events)
+
+
+def test_district_events_simple():
+    """Tests TBA's endpoint to retrieve shortened information about the events that occurred in a district."""
+    with ApiClient():
+        chs_district = District(2022, "chs")
+        assert chs_district.events() != chs_district.events(simple=True)
