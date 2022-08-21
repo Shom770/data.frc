b906eb7892912f8e7daed740737685f3f92d8653
Shom770 <82843611+Shom770@users.noreply.github.com>
2022-08-20 22:35:53 -0400

    event aliances test

--- src/tests/event_test.py
+++ src/tests/event_test.py
@@ -15,3 +15,12 @@ def test_event_kwarg():
     """Tests `Event` with ensuring that an instance is instantiated properly when the key is passed in as a keyword argument."""
     chs_comp = Event(key="2022chcmp")
     assert chs_comp.key == "2022chcmp"
+
+
+def test_event_alliances():
+    """Tests TBA's endpoint that retrieves all alliances in an event."""
+    chs_comp_alliances = Event("2022chcmp").alliances()
+    assert (
+        isinstance(chs_comp_alliances, Event.Alliance)
+        and all(isinstance(alliance, Event.Alliance) for alliance in chs_comp_alliances)
+    )
