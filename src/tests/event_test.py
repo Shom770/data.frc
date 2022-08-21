70494780d9bc025f914bb20185ad5a34c903c7ca
Shom770 <82843611+Shom770@users.noreply.github.com>
2022-08-20 22:33:02 -0400

    basic tests to start w for event

--- src/tests/event_test.py
+++ src/tests/event_test.py
@@ -5,4 +5,13 @@ from ..schemas import *
 from ..utils import *
 
 
-def test_
+def test_event_one_argument():
+    """Tests `Event` with ensuring that an instance is instantiated properly when only the key is passed in as a positional argument."""
+    chs_comp = Event("2022chcmp")
+    assert chs_comp.key == "2022chcmp"
+
+
+def test_event_kwarg():
+    """Tests `Event` with ensuring that an instance is instantiated properly when the key is passed in as a keyword argument."""
+    chs_comp = Event(key="2022chcmp")
+    assert chs_comp.key == "2022chcmp"
