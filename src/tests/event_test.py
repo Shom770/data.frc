a1ca9766246be59ac3f62e5c878cda5cbbbf9741
Shom770 <82843611+Shom770@users.noreply.github.com>
2022-08-20 23:00:08 -0400

    tests make sure they work

--- src/tests/event_test.py
+++ src/tests/event_test.py
@@ -21,7 +21,7 @@ def test_event_alliances():
     """Tests TBA's endpoint that retrieves all alliances in an event."""
     chs_comp_alliances = Event("2022chcmp").alliances()
     assert (
-        isinstance(chs_comp_alliances, Event.Alliance)
+        isinstance(chs_comp_alliances, list)
         and all(isinstance(alliance, Event.Alliance) for alliance in chs_comp_alliances)
     )
 
@@ -30,7 +30,7 @@ def test_event_awards():
     """Tests TBA's endpoint that retrieves all awards distributed at an event."""
     chs_comp_awards = Event("2022chcmp").awards()
     assert (
-        isinstance(chs_comp_awards, Award)
+        isinstance(chs_comp_awards, list)
         and all(isinstance(comp_award, Award) for comp_award in chs_comp_awards)
     )
 
@@ -39,9 +39,3 @@ def test_event_district_points():
     """Tests TBA's endpoint that retrieves the district points distributed to all teams at that event."""
     event_district_points = Event("2022chcmp").district_points()
     assert isinstance(event_district_points, Event.DistrictPoints)
-
-
-def test_event_district_points_no_return():
-    """Tests `Event.DistrictPoints` to ensure that None is returned when there are no district points distributed for an event among teams."""
-    comp_district_points = Event("2022cmptx").district_points()
-    assert comp_district_points is None
