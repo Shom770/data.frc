8ac46582efe4adb8a1f5c697aa084f5ffd4e83ad
Shom770 <82843611+Shom770@users.noreply.github.com>
2022-08-21 20:30:32 -0400

    black updating

--- src/utils/functions.py
+++ src/utils/functions.py
@@ -3,7 +3,7 @@ __all__ = ["construct_url"]
 
 def construct_url(base_endpoint, **kwargs) -> str:
     """
-    Constructs the URL with the given parameters.
+    Constructs a URL to send a request to with the given parameters.
 
     Parameters:
         endpoint: The base endpoint to add all the different additions to the URL.
@@ -12,13 +12,13 @@ def construct_url(base_endpoint, **kwargs) -> str:
     Returns:
         A string of the constructed URL based on the endpoints.
     """
-    return (
-            f"https://www.thebluealliance.com/api/v3/{base_endpoint}/" +
-            "/".join(
-                map(str, [
-                    param_name if isinstance(param_value, bool) else param_value
-                    for param_name, param_value in kwargs.items()
-                    if param_value is not None and param_value is not False
-                ])
-            )
+    return f"https://www.thebluealliance.com/api/v3/{base_endpoint}/" + "/".join(
+        map(
+            str,
+            [
+                param_name if isinstance(param_value, bool) else param_value
+                for param_name, param_value in kwargs.items()
+                if param_value is not None and param_value is not False
+            ],
+        )
     )
